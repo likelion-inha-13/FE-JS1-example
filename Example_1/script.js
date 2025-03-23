f2e03e101838d433b9bb6eacd2977bdde30c0692
thisjheok <jhjang3344@gmail.com>
2025-03-24 00:40:16 +0900

    title 변경

--- Example_1/script.js
+++ Example_1/script.js
@@ -1,4 +1,7 @@
 const red = document.getElementById('red');
 
+console.log("red.parentNode: ",red.parentNode);
+console.log("red.parentElement : ",red.parentElement );
+
 console.log("red.childNodes: ",red.childNodes);
 console.log("red.children: ",red.children); 
